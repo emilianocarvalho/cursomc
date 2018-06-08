6ea210c6d420b4c5a230f0be56775ee9e9ae27c5
Emiliano Carvalho <emilianocarvalho.dev@gmail.com>
2018-06-08 12:44:34 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/emilianocarvalho/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/emilianocarvalho/cursomc/resource/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.emilianocarvalho.cursomc.resource;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
